b06f245bcb591191b343984089f7e418a64b1adf
sibest19 <andreanisimone@gmail.com>
2016-04-13 09:54:08 +0200

    Removed some unused entries

--- brew.sh
+++ brew.sh
@@ -22,9 +22,9 @@ brew install gnu-sed --with-default-names
 # Install Bash 4.
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 # running `chsh`.
-brew install bash
-brew tap homebrew/versions
-brew install bash-completion2
+# brew install bash
+# brew tap homebrew/versions
+# brew install bash-completion2
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
